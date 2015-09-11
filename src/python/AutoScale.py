c46bbebd59a1cab8f19b371be1d8f9480baba5ec
Marco Mascheroni <marco.mascheroni@cern.ch>
2015-09-11 11:45:59 +0200

    PEP-0008 compliance: a first step
    I used this to format the code http://pythoniter.appspot.com/
    I am not sure this compies PEP-0008 but it is a first step

--- src/python/AutoScale.py
+++ src/python/AutoScale.py
@@ -2,31 +2,37 @@ import time
 import htcondor
 from Plugins.AwsEC2 import AwsEC2
 from Functions.Additional import parse_args, getIdleJobs, get_config
-    
+
+
 def main():
-    #Prepared to pass arguments file with defined parameters
-    opts, args = parse_args()
+
+    # Prepared to pass arguments file with defined parameters
+
+    (opts, args) = parse_args()
     config = get_config(opts.config)
-    
+
     coll = htcondor.Collector()
     autoType = 't1.micro'
-    
-    #Key name must be always unique. Adding timestamp
+
+    # Key name must be always unique. Adding timestamp
+
     aws = AwsEC2(**config)
     aws.checkStatus()
-    aws.createInstance("ami-7fc59d4f", 1, "t1.micro", True)
+    aws.createInstance('ami-7fc59d4f', 1, 't1.micro', True)
     while True:
         IdleJobInfo = {}
         print 'Querying collector and getting schedulers...'
-        schedd_ads = coll.query(htcondor.AdTypes.Schedd, '', ['Name', 'MyAddress', 'ScheddIpAddr'])
+        schedd_ads = coll.query(htcondor.AdTypes.Schedd, '', ['Name',
+                                'MyAddress', 'ScheddIpAddr'])
         for ad in schedd_ads:
             print 'Querying schedds and getting idle jobs'
             IdleJobInfo = getIdleJobs(ad, IdleJobInfo, autoType)
         print 'Queries done. If i have idle jobs, will proceed requesting VM`s'
         for group in IdleJobInfo.keys():
             for vmtype in IdleJobInfo[group].keys():
-                print '---- Group %s, VMType %s, IdleJobs %s' % (group, vmtype, IdleJobInfo[group][vmtype])
-                if group == "None":
+                print '---- Group %s, VMType %s, IdleJobs %s' % (group,
+                        vmtype, IdleJobInfo[group][vmtype])
+                if group == 'None':
                     print '------ Found group None!'
                 else:
                     print '------ Time to boot up some VM`s'
@@ -34,6 +40,7 @@ def main():
         aws.checkStatus()
         print 'Waiting 500s for next cycle...'
         time.sleep(5)
+
 
 if __name__ == '__main__':
     main()
--- src/python/Functions/Additional.py
+++ src/python/Functions/Additional.py
@@ -1,59 +1,67 @@
-import re
 import os
 import sys
 import json
-import time
 import optparse
 import htcondor
-import ConfigParser
+
 
 def get_config(config):
     if config:
         if not os.path.exists(config):
-            print >> sys.stderr, "Config file %s does not exist." % config
+            print >> sys.stderr, 'Config file %s does not exist.' \
+                % config
             sys.exit(1)
         with open(config) as fd:
             return json.load(fd)
-    elif os.path.exists("config.json"):
-        with open("config.json") as fd:
+    elif os.path.exists('config.json'):
+        with open('config.json') as fd:
             return json.load(fd)
+
 
 def parse_args():
     """Parse additional arguments"""
+
     parser = optparse.OptionParser()
-    parser.add_option("-c", "--config", help="Configuration file", dest="config", default=None)
-    opts, args = parser.parse_args()
+    parser.add_option('-c', '--config', help='Configuration file',
+                      dest='config', default=None)
+    (opts, args) = parser.parse_args()
     if args:
         parser.print_help()
-        print >> sys.stderr, "%s takes no arguments." % sys.args[0]
+        print >> sys.stderr, '%s takes no arguments.' % sys.args[0]
         sys.exit(1)
 
-    return opts, args
+    return (opts, args)
+
 
 def get_logger():
     """Create logging instance"""
+
     return True
+
 
 def getIdleJobs(ad, IdleJobInfo, autoType):
     schedd = htcondor.Schedd(ad)
     try:
-        jobs = schedd.xquery("JobStatus=?=1", ["RequestMemory", "MaxWallTimeMins", "QDate", "AutoCloudType", "AutoCloudGroup"])
+        jobs = schedd.xquery('JobStatus=?=1', ['RequestMemory',
+                             'MaxWallTimeMins', 'QDate', 'AutoCloudType'
+                             , 'AutoCloudGroup'])
     except Exception, e:
-        print "Failed querying", ad["Name"]
+        print 'Failed querying', ad['Name']
         print e
         return
     for job in jobs:
-	autoCloudGroup = "None"
-	autoCloudType = autoType
-	if 'AutoCloudType' in job.keys():
-	    autoCloudType = job['AutoCloudType']
-	if 'AutoCloudGroup' in job.keys():
-	    autoCloudGroup = job['AutoCloudGroup']
+        autoCloudGroup = 'None'
+        autoCloudType = autoType
+        if 'AutoCloudType' in job.keys():
+            autoCloudType = job['AutoCloudType']
+        if 'AutoCloudGroup' in job.keys():
+            autoCloudGroup = job['AutoCloudGroup']
 
-	#Double checking that it does not exists and add what is needed
-	if autoCloudGroup not in IdleJobInfo.keys():
+    # Double checking that it does not exists and add what is needed
+
+        if autoCloudGroup not in IdleJobInfo.keys():
             IdleJobInfo[autoCloudGroup] = {}
-	if autoCloudType not in IdleJobInfo[autoCloudGroup].keys():
-	    IdleJobInfo[autoCloudGroup][autoCloudType] = 0
-	IdleJobInfo[autoCloudGroup][autoCloudType] += 1
+        if autoCloudType not in IdleJobInfo[autoCloudGroup].keys():
+            IdleJobInfo[autoCloudGroup][autoCloudType] = 0
+        IdleJobInfo[autoCloudGroup][autoCloudType] += 1
     return IdleJobInfo
